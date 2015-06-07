c9399e1cedfa37b6f428f6fc895b51507010ef26
Michael Goerz <goerz@physik.uni-kassel.de>
2015-06-08 00:00:31 +0200

    Ensure wc>w2

--- run_stage1.py
+++ run_stage1.py
@@ -47,6 +47,8 @@ def main():
         log.write("%s\n" % time.asctime())
         for w2 in [6.0, 6.25, 6.5, 6.75, 7.0, 7.25, 7.5, 7.75, 8.0]:
             for wc in [6.6, 7.1, 7.6, 8.1, 8.6, 9.1, 9.6, 10.1, 10.6, 11.1]:
+                if wc <= w2:
+                    continue
                 jobname = 'w2_%dMHz_wc_%dMHz_stage1' % (w2*1000, wc*1000)
                 job = Job(jobscript=jobscript(w2, wc), jobname=jobname,
                         workdir='.', time='8:00:00', nodes=1, threads=12,
