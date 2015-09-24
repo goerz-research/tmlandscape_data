180d398c2ecc4dea99dda9556a1efbb057f5959a
Michael Goerz <goerz@physik.uni-kassel.de>
2015-09-23 22:06:36 -0400

    Bug fix in closest_targets.py; enabled debug output
    Only the first 5 SQ gates would be calculated per run!

--- closest_targets.py
+++ closest_targets.py
@@ -50,8 +50,9 @@ def make_threadpool_map(p):
     return threadpool_map
 
 
-def get_closest_targets(runs):
+def get_closest_targets(runs, dry_run=False):
     thread_pool_map = make_threadpool_map(get_cpus())
+    logger = logging.getLogger()
     for stage in ['stage2', 'stage3']:
         PE_gate_files = []
         SQ_gate_files = []
@@ -65,25 +66,31 @@ def get_closest_targets(runs):
                 U_dat = os.path.join(folder, subfolder, 'U.dat')
                 U_closest_dat = os.path.join(folder, subfolder,
                                              'U_closest_%s.dat'%target)
-                if os.path.isfile(U_dat) and not os.path.isfile(U_closest_dat):
-                    if target == 'PE':
-                        PE_gate_files.append(U_dat)
+                if os.path.isfile(U_dat):
+                    if not os.path.isfile(U_closest_dat):
+                        if target == 'PE':
+                            logger.debug("Require closest PE gate for %s",
+                                         U_dat)
+                            PE_gate_files.append(U_dat)
+                        else:
+                            logger.debug("Require closest SQ gate for %s",
+                                         U_dat)
+                            SQ_gate_files.append(U_dat)
                     else:
-                        SQ_gate_files.append(U_dat)
-        # Note: memoization does NOT work with parallel execution (because a
-        # thread pool has no shared memory)
-        # process SQ gates
-        QDYN.gate2q.closest_SQ.clear()
-        SQ_cache_file = os.path.join(runs, "closest_SQ_%s.cache"%stage)
-        QDYN.gate2q.closest_SQ.load(SQ_cache_file)
-        thread_pool_map(get_closest_SQ, SQ_gate_files[:5])
-        QDYN.gate2q.closest_SQ.dump(SQ_cache_file)
-        # process PE gates
-        QDYN.gate2q.closest_PE.clear()
-        PE_cache_file = os.path.join(runs, "closest_PE_%s.cache"%stage)
-        QDYN.gate2q.closest_PE.load(PE_cache_file)
-        thread_pool_map(get_closest_PE, PE_gate_files)
-        QDYN.gate2q.closest_PE.dump(PE_cache_file)
+                        logger.debug("Skipping %s, closest gate already "
+                                     "present", U_dat)
+                else:
+                    logger.debug("Skipping %s, does not exist", U_dat)
+        if dry_run:
+            for gate_file in SQ_gate_files:
+                print("get_closests_SQ(%s)" % gate_file)
+            for gate_file in PE_gate_files:
+                print("get_closests_PE(%s)" % gate_file)
+        else:
+            # Note: memoization does NOT work with parallel execution (because
+            # a thread pool has no shared memory)
+            thread_pool_map(get_closest_SQ, SQ_gate_files)
+            thread_pool_map(get_closest_PE, PE_gate_files)
 
 
 # workers:
@@ -94,13 +101,16 @@ def get_closest_PE(gatefile):
     except Exception as e:
         print("ERROR %s: %s"%(gatefile, e))
 
+
 def get_closest_SQ(gatefile):
     try:
         get_closest(gatefile, 'SQ')
     except Exception as e:
         print("ERROR %s: %s"%(gatefile, e))
 
-def get_closest(gatefile, target):
+
+def get_closest(gatefile, target, raise_exceptions=False):
+    logger = logging.getLogger()
     U = QDYN.gate2q.Gate2Q(file=gatefile)
     root, ext = os.path.splitext(gatefile)
     outfile = root + "_closest_%s"%target + ext
@@ -114,9 +124,14 @@ def get_closest(gatefile, target):
         try:
             U_closest = f_closest(U.closest_unitary())
         except ValueError:
-            return
+            logger.warn("Could not find closest unitary for %s", gatefile)
+            if raise_exceptions:
+                raise
+            else:
+                return None
     with open(outfile, 'w') as out_fh:
         QDYN.io.print_matrix(U_closest, out=out_fh)
+    return U_closest
 
 
 def main(argv=None):
@@ -127,12 +142,23 @@ def main(argv=None):
     arg_parser = OptionParser(
     usage = "%prog [options] <runs>",
     description = __doc__)
+    arg_parser.add_option(
+        '--debug', action='store_true', dest='debug',
+        default=False, help="Enable debugging output")
+    arg_parser.add_option(
+        '-n', action='store_true', dest='dry_run',
+        default=False, help="Perform a dry run")
     options, args = arg_parser.parse_args(argv)
+    logger = logging.getLogger()
+    if options.debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
     try:
         runs = args[1]
     except IndexError:
         arg_parser.error("Missing arguments")
-    get_closest_targets(runs)
+    get_closest_targets(runs, dry_run=options.dry_run)
 
 if __name__ == "__main__":
     sys.exit(main())
